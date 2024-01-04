37b42ec9a6d4895cb0b6b2c0335c9976843cc988
albertng94 <albertng94@gmail.com>
2024-01-04 20:52:51 +0100

    Add book object/div creation functionality
    Function book is an Object Constructor containing book properties:
    title, author, pages, and read.
    
    Array myLibrary is created. It stores the book objects created by the
    user.
    
    submitBookButton() function triggers whenever the user clicks the create
    button within the "Add new book" dialog. The following functionalities
    have been added to the function (more to come):
    
    --> push a new book object to the myLibrary Array. The object contains
    the properties according to the users form completion.
    
    --> close the dialog
    
    --> loop through myLibraryArray (with stored objects). For each object
    stored, create a div, give it an id equal to the object array's index,
    give it "grid-item" class so it can be styled through css, and append it.
    This will create as many divs as objects the user creates.

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ closeBook.addEventListener("click", () => {
 });
 
 
-// Function to create new books:
+// Book Object Constructor - stores the basic information to be added from each book.
 
 function book(title, author, pages, read) {
     this.title = title;
@@ -22,3 +22,50 @@ function book(title, author, pages, read) {
     this.pages = pages;
     this.read = read;
 }
+
+// Array myLibrary stores all book objects
+
+const myLibrary = [];
+
+
+// Variables used by the submitBookButton() function.
+
+const submitBookButton = document.getElementById("submit-book");
+const bookTitle = document.getElementById("title");
+const bookAuthor = document.getElementById("author");
+const bookPages = document.getElementById("pages");
+const bookRead = document.getElementById("read");
+const grid = document.getElementById("grid");
+
+
+// Counter variable is used by submitBookButton keep track of the books number ((created - deleted)-1)
+
+let counter = -1;
+
+
+// Add new book to library when the necessary button is clicked
+submitBookButton.addEventListener("click", () => {
+    // Push new book into myLibrary Array
+    myLibrary.push(new book(bookTitle.value, bookAuthor.value, bookPages.value, bookRead.checked));
+    // Close dialog
+    bookDialog.close();
+
+    // Loop through myLibrary Array
+    for (let i = counter; i < myLibrary.length-1; i++) {
+        // Create a div, 
+        // add an id attribute equal to counter+1 (id is the index of the book within myLibrary Array), 
+        // add the class "grid-item" to style the div through css, 
+        // append the div, 
+        // increment counter (keeping track of the exact number of books)
+        const bookDiv = document.createElement("div");
+        bookDiv.setAttribute("id", `${counter + 1}`)
+        bookDiv.classList.add("grid-item");
+        grid.appendChild(bookDiv);
+        counter++;
+    }
+   
+});
+
+
+// Añadir los sub-elementos y el styling para los .grid-item 
+// cuando la array se queda con 0 objetos, habrá que hacer un counter-- para volverlo a dejar en -1. No puede quedar en 0.
